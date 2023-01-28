bb57f6b31a37d1bef47ca085d6d5f4ec5522d298
SATHEESH <sathishadipad1998@gmail.com>
2023-01-29 00:29:42 +0530

    intial commit

---
+++ src/pages/gallery/index.jsx
@@ -0,0 +1,17 @@
+import Header from "../../components/header";
+import SiteTitle from "../../components/common/siteTitle";
+import Layout from "../../components/layout";
+import ComingSoon from "../../components/common/comingSoon";
+function Gallery() {
+  return (
+    <>
+      <SiteTitle title="gallery" />
+      <Layout>
+        <Header heading={"Gallery"} />
+        <ComingSoon />
+      </Layout>
+    </>
+  );
+}
+
+export default Gallery;
---
+++ src/pages/index.jsx
@@ -0,0 +1,10 @@
+import LandingPage from "../components/ladingPage";
+function LandingPages() {
+    return ( <>
+    <div style={{height:'100vh'}}>
+        <LandingPage />
+    </div>
+    </> );
+}
+
+export default LandingPages;
